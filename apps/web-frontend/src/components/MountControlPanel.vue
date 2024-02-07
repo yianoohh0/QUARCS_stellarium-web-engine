01d843f41f5e48401381f161a564fc941c51b8d3
astro <QHYCCD-QUARCS>
2024-02-07 14:15:21 +0800

    Improve control panel of the mount

--- apps/web-frontend/src/components/MountControlPanel.vue
+++ apps/web-frontend/src/components/MountControlPanel.vue
@@ -16,9 +16,16 @@
       <button class="custom-button dec-plus no-select" @mousedown="handleMouseDownDEC('plus')" @mouseup="stop" >DEC +</button>
       <button class="custom-button dec-minus no-select" @mousedown="handleMouseDownDEC('minus')" @mouseup="stop">DEC -</button>
     </div> -->
-    <div class="Dial-Knob" :style="{ transform: `rotate(${angle}deg)` }">
-      <div class="indicator" @mousedown="startDrag" @touchstart="startDragMobile"></div>
+
+    <div class="Dial-Knob">
+      <div class="tick-marks">
+        <div class="tick-mark" v-for="(angle, index) in snapAngles" :key="index" :style="{ transform: `rotate(${angle}deg)` }"></div>
+      </div>
+      <div class="knob" :style="{ transform: `rotate(${angle}deg)` }">
+        <div class="indicator" @mousedown="startDrag" @touchstart="startDragMobile">{{ speedNum }}</div>
+      </div>
     </div>
+
     <div>
       <button class="btn-stop no-select" @touchend="stop">STOP</button>
     </div>
@@ -34,8 +41,8 @@
     </div>
     
       <div v-if="showButtons">
-        <button class="custom-button btn-park no-select" @touchend="MountPark">Park</button>
-        <button class="custom-button btn-truck no-select" @touchend="MountTruck">Truck</button>
+        <button v-bind:class="{ 'btn-park-on no-select': ParkSwitch, 'btn-park no-select': !ParkSwitch }" @touchend="MountPark">Park</button>
+        <button v-bind:class="{ 'btn-track-on no-select': TrackSwitch, 'btn-track no-select': !TrackSwitch }" @touchend="MountTrack">Track</button>
         <button class="custom-button btn-home no-select" @touchend="MountHome">Home</button>
         <button class="custom-button btn-sync no-select" @touchend="MountSYNC">SYNC</button>
       </div>
@@ -61,16 +68,31 @@ export default {
       
       isExpanded: false, // 新增状态来追踪面板是否展开
       showButtons: false,
+
+      ParkSwitch: false,
+      TrackSwitch: false,
+
+      // angle: 0,
+      // dragging: false,
+      // startAngle: 0,
+      snapAngles: [], // 定义吸附的角度位置
+      speedNum: 0,
     };
+  },
+  created() {
+    this.generateSnapAngles(360);
+    this.$bus.$on('MountParkSwitch', this.MountParkSwitch);
+    this.$bus.$on('MountTrackSwitch', this.MountTrackSwitch);
+    this.$bus.$on('MountTotalSlewRate',this.generateSnapAngles);
   },
   methods: {
     handleMouseDownRA(direction) {
       // 在这里实现调整RA的逻辑
       console.log(`QHYCCD | 调整RA ${direction}`);
       if(direction === 'plus'){
-        this.$bus.$emit('AppSendMessage', 'Vue_Command', 'N');
+        this.$bus.$emit('AppSendMessage', 'Vue_Command', 'MountMoveNorth');
       } else {
-        this.$bus.$emit('AppSendMessage', 'Vue_Command', 'S');
+        this.$bus.$emit('AppSendMessage', 'Vue_Command', 'MountMoveSouth');
       }
       
     },
@@ -78,17 +100,33 @@ export default {
       // 在这里实现调整DEC的逻辑
       console.log(`QHYCCD | 调整DEC ${direction}`);
       if(direction === 'plus'){
-        this.$bus.$emit('AppSendMessage', 'Vue_Command', 'W');
+        this.$bus.$emit('AppSendMessage', 'Vue_Command', 'MountMoveWest');
       } else {
-        this.$bus.$emit('AppSendMessage', 'Vue_Command', 'E');
+        this.$bus.$emit('AppSendMessage', 'Vue_Command', 'MountMoveEast');
       }
     },
     stop() {
       // 在这里实现停止的逻辑
       console.log('QHYCCD | 停止');
-      this.$bus.$emit('AppSendMessage', 'Vue_Command', 'A');
+      this.$bus.$emit('AppSendMessage', 'Vue_Command', 'MountMoveAbort');
     },
 
+    MountPark() {
+      console.log('QHYCCD | Park');
+      this.$bus.$emit('AppSendMessage', 'Vue_Command', 'MountPark');
+    },
+    MountTrack() {
+      console.log('QHYCCD | Truck');
+      this.$bus.$emit('AppSendMessage', 'Vue_Command', 'MountTrack');
+    },
+    MountHome() {
+      console.log('QHYCCD | Home');
+      this.$bus.$emit('AppSendMessage', 'Vue_Command', 'MountHome');
+    },
+    MountSYNC() {
+      console.log('QHYCCD | SYNC');
+      this.$bus.$emit('AppSendMessage', 'Vue_Command', 'MountSYNC');
+    },
     
     startDrag(event) {
       this.dragging = true;
@@ -105,6 +143,15 @@ export default {
     },
     stopDrag() {
       this.dragging = false;
+      // 在拖拽结束时吸附到最近的角度位置
+      const nearestAngle = this.findNearestAngle(this.angle);
+      this.angle = nearestAngle;
+      
+       // 获取当前档数
+       this.speedNum = this.snapAngles.indexOf(nearestAngle) + 1;
+       console.log('当前档数：', this.speedNum);
+       this.$bus.$emit('AppSendMessage', 'Vue_Command', 'MountSpeedSet:'+this.speedNum);  
+      
       document.removeEventListener("mousemove", this.onDrag);
       document.removeEventListener("mouseup", this.stopDrag);
     },
@@ -135,8 +182,43 @@ export default {
 
     stopDragMobile() {
       this.dragging = false;
+      // 在触摸拖拽结束时吸附到最近的角度位置
+      const nearestAngle = this.findNearestAngle(this.angle);
+      this.angle = nearestAngle;
+      
+      // 获取当前档数
+      this.speedNum = this.snapAngles.indexOf(nearestAngle) + 1;
+      console.log('当前档数：', this.speedNum);    
+      this.$bus.$emit('AppSendMessage', 'Vue_Command', 'MountSpeedSet:'+this.speedNum);  
+      
       document.removeEventListener("touchmove", this.onDragMobile);
       document.removeEventListener("touchend", this.stopDragMobile);
+    },
+
+    findNearestAngle(angle) {
+      // 处理角度值在0到360度之间
+      angle = (angle % 360 + 360) % 360;
+
+      let nearestAngle = this.snapAngles[0];
+      let minDifference = Math.abs(angle - nearestAngle);
+      for (let i = 1; i < this.snapAngles.length; i++) {
+        const difference = Math.abs(angle - this.snapAngles[i]);
+        if (difference < minDifference) {
+          minDifference = difference;
+          nearestAngle = this.snapAngles[i];
+        }
+      }
+      return nearestAngle;
+    },
+    generateSnapAngles(num) {
+      // 生成 num 个吸附角度，将其均匀分布在360度内
+      console.log('generateSnapAngles:',num);
+      this.snapAngles = [];
+      const interval = 360 / num;
+      for (let i = 0; i < num; i++) {
+        this.snapAngles.push(i * interval);
+        console.log('pushAngle:',i * interval);
+      }
     },
     
     toggleMore() {
@@ -153,18 +235,25 @@ export default {
         this.height = 180;
       }
     },
-    
-    MountPark() {
-      console.log('QHYCCD | Park');
+    MountParkSwitch(Switch) {
+      if(Switch === 'ON')
+      {
+        this.ParkSwitch = true;
+      }
+      else
+      {
+        this.ParkSwitch = false;
+      }
     },
-    MountTruck() {
-      console.log('QHYCCD | Truck');
-    },
-    MountHome() {
-      console.log('QHYCCD | Home');
-    },
-    MountSYNC() {
-      console.log('QHYCCD | SYNC');
+    MountTrackSwitch(Switch) {
+      if(Switch === 'ON')
+      {
+        this.TrackSwitch = true;
+      }
+      else
+      {
+        this.TrackSwitch = false;
+      }
     },
   }
 }
@@ -172,11 +261,9 @@ export default {
 
 <style scoped>
 .mount-control-panel {
-  /* z-index: 1; */
   pointer-events: auto;
   position: fixed;
   background-color: rgba(128, 128, 128, 0.5);
-  /* border: 1px solid rgba(255, 255, 255, 0.8); */
   backdrop-filter: blur(5px);
   border-radius: 10px;
   box-sizing: border-box;
@@ -186,10 +273,9 @@ export default {
 .custom-button {
   user-select: none;
   background-color: rgba(0, 0, 0, 0.3);
-  /* border: 1px solid rgba(255, 255, 255, 0.8); */
-  backdrop-filter: blur(5px); /* 添加毛玻璃效果 */
-  border-radius: 5px; /* 添加圆角效果，根据需要调整圆角半径 */
-  box-sizing: border-box; /* 设置box-sizing为border-box */
+  backdrop-filter: blur(5px);
+  border-radius: 5px;
+  box-sizing: border-box;
 }
 
 .ra-plus {
@@ -265,6 +351,8 @@ export default {
 .btn-more:active,
 .btn-park:active,
 .btn-truck:active,
+.btn-park-on:active,
+.btn-truck-on:active,
 .btn-home:active,
 .btn-sync:active {
   transform: scale(0.95); /* 点击时缩小按钮 */
@@ -281,50 +369,64 @@ export default {
 }
 
 .Dial-Knob {
+  width: 100px;
+  height: 100px;
+  top: 25px;
+  left: 25px;
+  position: relative;
+}
 
+.tick-marks {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: -48px;
+  transform-origin: center center;
+  transform: rotate(90deg);
+}
+
+.tick-mark {
+  position: absolute;
+  width: 4px; /* 刻度线的宽度 */
+  height: 4px; /* 刻度线的高度 */
+  border-radius: 50%;
+  background-color: rgba(0, 0, 0, 0.5);
+  transform-origin: 50px; /* 以刻度线顶部为旋转中心 */
+}
+
+.knob {
+  width: 100px;
+  height: 100px;
+  background-color: rgba(50, 50, 50, 0.5);
   border-radius: 50%;
   display: flex;
   justify-content: center;
   align-items: center;
   cursor: pointer;
   user-select: none;
-  position:absolute;
-  width: 100px;
-  height: 100px;
-  top: 25px;
-  left: 25px;
-  
-  background-color: rgba(0, 0, 0, 0.1);
-  /* border: 1px solid rgba(255, 255, 255, 0.8); */
-  backdrop-filter: blur(5px); /* 添加毛玻璃效果 */
-  box-sizing: border-box;
+  position: relative;
 }
-/*
-.Dial-Knob::before {
-  content: "";
-  position: absolute;
-  width: 2px;
-  height: 2px;
-  background-color: red;
-  border-radius: 50%;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-}*/
 
 .indicator {
-  width: 24px;
-  height: 24px;
-
+  width: 25px;
+  height: 25px;
   border-radius: 50%; /* 添加圆角半径属性 */
   position: absolute;
-  top: 0.5px; /* 调整垂直位置 */
+  top: 0px; /* 调整垂直位置 */
   left: 50%;
   transform: translateX(-50%);
-  
+  /* transform: translate(-50%, -50%); */
   background-color: rgba(0, 0, 0, 0.5);
-  /* border: 1px solid rgba(255, 255, 255, 0.8); */
+  border: 1px solid rgba(0, 0, 0, 0.3);
   backdrop-filter: blur(5px); /* 添加毛玻璃效果 */
+  cursor: pointer;
+  user-select: none;
+  box-sizing: border-box;
+
+  color: rgba(0, 0, 0, 0.3);
+  font-size: 16px;
+  text-align: center;
 }
 
 .btn-more {
@@ -341,14 +443,54 @@ export default {
   height: 35px;
   bottom: 70px;
   left: 4px;
+
+  user-select: none;
+  background-color: rgba(0, 0, 0, 0.3);
+  backdrop-filter: blur(5px);
+  border-radius: 5px;
+  box-sizing: border-box;
 }
 
-.btn-truck{
+.btn-track{
   position:absolute;
   width: 70px;
   height: 35px;
   bottom: 70px;
   right: 4px;
+
+  user-select: none;
+  background-color: rgba(0, 0, 0, 0.3);
+  backdrop-filter: blur(5px);
+  border-radius: 5px;
+  box-sizing: border-box;
+}
+
+.btn-park-on{
+  position:absolute;
+  width: 70px;
+  height: 35px;
+  bottom: 70px;
+  left: 4px;
+
+  user-select: none;
+  background-color: rgba(0, 255, 30, 0.5);
+  backdrop-filter: blur(5px);
+  border-radius: 5px;
+  box-sizing: border-box;
+}
+
+.btn-track-on{
+  position:absolute;
+  width: 70px;
+  height: 35px;
+  bottom: 70px;
+  right: 4px;
+
+  user-select: none;
+  background-color: rgba(0, 255, 30, 0.5);
+  backdrop-filter: blur(5px);
+  border-radius: 5px;
+  box-sizing: border-box;
 }
 
 .btn-home{
--- apps/web-frontend/src/components/toolbar.vue
+++ apps/web-frontend/src/components/toolbar.vue
@@ -11,7 +11,7 @@
     <v-toolbar class="transparent" dense>
       <v-app-bar-nav-icon @click="toggleNavigationDrawer"></v-app-bar-nav-icon>
       <img class="tbtitle hidden-xs-only" id="stellarium-web-toolbar-logo" src="@/assets/images/logo.svg" width="30" height="30" alt="Stellarium Web Logo"/>
-      <span class="tbtitle">Stellarium<sup>Web</sup></span>
+      <span class="tbtitle">Q.U.A.R.C.S</span>
       <v-spacer></v-spacer>
       <target-search></target-search>
       <v-spacer></v-spacer>
